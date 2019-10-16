73469b84ac4469c8da7dcebf8c515f9b76d3b54e
Alan James <alanjames@hotmail.com>
2019-10-16 16:05:14 +1100

    Initial commit

---
+++ CMCApi/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using CMCApi.Models;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CMCApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/products/")]
+        public IHttpActionResult GetAllProducts()
+        {
+            using (StreamReader r = new StreamReader(HttpContext.Current.Server.MapPath(@"~/App_Data/productdata.json")))
+            {
+                string json = r.ReadToEnd();
+                List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
+                return Ok(products);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/products/shipping/{centstotal}")]
+        public IHttpActionResult CalculateShipping(int centstotal)
+        {
+            int shippingcents = 1000;
+            if (centstotal >= 5000)
+                shippingcents = 2000;
+            return Ok(shippingcents);
+        }
+
+        [HttpPost]
+        [Route("api/products/placeorder/")]
+        public IHttpActionResult PlaceOrder([FromBody] List<Product> products)
+        {
+            if (products.Count() == 0)
+                return NotFound();
+            else
+            {
+                foreach (Product product in products)
+                {
+                    //do something
+                }
+                return Ok();
+            }
+        }
+
+
+
+    }
+}
---
+++ CMCApi/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMCApi.Models
+{
+    public class Product
+    {
+        public int product_id { get; set; }
+        public string productname { get; set; }
+        public string product_guid { get; set; }
+        public decimal price { get; set; }
+        public int available { get; set; }
+
+    }
+}
